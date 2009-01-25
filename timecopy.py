5f6a3a1db01679e489ec63a6173c2cf6c0976a5c
Nathan Fiedler <nathanfiedler@gmail.com>
2009-01-25 22:57:30 +0000

    Partial implementation of timecopy Python script, not yet working.

--- timecopy.py
+++ timecopy.py
@@ -17,30 +17,32 @@
 #
 
 #####################################################################
-###    WORK IN PROGRESS -- WORK IN PROGRESS -- WORK IN PROGRESS   ###
+#XXX   WORK IN PROGRESS -- WORK IN PROGRESS -- WORK IN PROGRESS   ###
 #####################################################################
-
-# TODO
+#
+# TODO EVENTUALLY
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# - Determine how to create hard links
-# - Write directory tree constructor to replicate source
-# - Copy files from source
-#   - Copy bytes
-#   - Set owner/group
-#   - Set permissions
-#   - Set accessed/modified times (utime() function)
+# - Implement verbose mode
+# - Implement dry-run mode
 # - Allow resuming a copy of a directory tree
-#   - Only copy what hasn't been done already
 #   - Check for incomplete copies
+#   - Only copy what hasn't been done already
 # - Recover from errors
 #   - Attempt the operation 3 times before treating as an error
 #   - Keep going despite errors
+# - Detect presence of multiple hosts in backup, prompt user for
+#   which ones to backup (allow backing up all)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+import getopt
 import os
+import re
+import shutil
 import sys
 from stat import *
 
 class TreeVisitor:
-    """Visitor pattern for walktree function. As tree is traversed,
+    """Visitor pattern for visitfiles function. As tree is traversed,
     methods of the visitor are invoked."""
 
     def dir(self, dir):
@@ -55,11 +57,8 @@ class TreeVisitor:
         """A symbolic link has been encountered."""
         pass
 
-def walktree(dir, visitor):
-    """Recursively descend the directory tree rooted at dir,
-       calling the visitor for each entry encountered. The tree
-       is traversed in depth-first order, which is most space
-       efficient and lends itself well to directory operations."""
+def visitfiles(dir, visitor):
+    "Calls the visitor for each entry encountered in the directory."
 
     for entry in os.listdir(dir):
         pathname = os.path.join(dir, entry)
@@ -76,24 +75,189 @@ def walktree(dir, visitor):
         except OSError:
             print 'Error reading %s' % pathname
 
-class BasicTreeVisitor(TreeVisitor):
+class CopyInitialVisitor(TreeVisitor):
+    """Copies a directory tree from one place to another."""
+
+    def copytree(self, src, dst):
+        """Copies the directory tree rooted at src to dst."""
+        self.src = src
+        self.dst = dst
+        visitfiles(src, self)
+
     def dir(self, dir):
-        walktree(dir, self)
+        # Create destination directory, copying stats and ownership.
+        dst = re.sub(self.src, self.dst, dir)
+        os.mkdir(dst)
+        shutil.copystat(dir, dst)
+        stats = os.lstat(dir)
+        os.lchown(dst, stats[ST_UID], stats[ST_GID])
+        # Continue traversal...
+        visitfiles(dir, self)
 
     def file(self, file):
-        print file
+        dst = re.sub(self.src, self.dst, file)
+        # Copy file contents from snapshot to destination.
+        shutil.copyfile(file, dst)
+        # Copy the permissions and accessed/modified times.
+        shutil.copystat(file, dst)
+        # Copy the owner/group values to destination.
+        stats = os.lstat(file)
+        os.lchown(dst, stats[ST_UID], stats[ST_GID])
 
     def link(self, link):
-        print '%s@' % link
+        # Copy link to destination.
+        lnk = os.readlink(link)
+        dst = re.sub(self.src, self.dst, link)
+        os.symlink(lnk, dst)
+        stats = os.lstat(link)
+        os.lchown(dst, stats[ST_UID], stats[ST_GID])
+
+class CopyBackupVisitor(TreeVisitor):
+    """Copies a directory tree and its files, where those entries differ
+       from a reference tree. That is, if any entry has the same inode
+       value as the corresponding entry in the reference tree, a new
+       hard link is made in the destination, and nothing further is
+       done with that entry (directories are not traversed, files are
+       not copied)."""
+
+    def __init__(self, old):
+        "old is the reference tree to which src will be compared"
+        self.old = old
+
+    def copytree(self, src, dst):
+        "Copy the tree rooted at src to dst"
+        self.src = src
+        self.dst = dst
+        visitfiles(src, self)
+
+    def dir(self, dir):
+        stats = os.lstat(dir)
+        old = re.sub(self.src, self.old, dir)
+        ostats = os.lstat(old)
+        dst = re.sub(self.src, self.dst, dir)
+        if stats[ST_INO] != ostats[ST_INO]:
+            # Create destination directory, copying stats and ownership.
+            os.mkdir(dst)
+            shutil.copystat(dir, dst)
+            os.lchown(dst, stats[ST_UID], stats[ST_GID])
+            # Continue traversal...
+            visitfiles(dir, self)
+        else:
+            # Create hard link in destination.
+            odst = re.sub(self.old, self.dst, dir)
+            os.link(dst, odst)
+
+    def file(self, file):
+        stats = os.lstat(file)
+        old = re.sub(self.src, self.old, file)
+        ostats = os.lstat(old)
+        if stats[ST_INO] != ostats[ST_INO]:
+            dst = re.sub(self.src, self.dst, file)
+            # Copy file contents from snapshot to destination.
+            shutil.copyfile(file, dst)
+            # Copy the permissions and accessed/modified times.
+            shutil.copystat(file, dst)
+            # Copy the owner/group values to destination.
+            os.lchown(dst, stats[ST_UID], stats[ST_GID])
+        else:
+            # Create hard link in destination.
+            odst = re.sub(self.old, self.dst, file)
+            os.link(dst, odst)
+
+    def link(self, link):
+        stats = os.lstat(link)
+        old = re.sub(self.src, self.old, link)
+        ostats = os.lstat(old)
+        if stats[ST_INO] != ostats[ST_INO]:
+            # Copy link to destination.
+            lnk = os.readlink(link)
+            dst = re.sub(self.src, self.dst, link)
+            os.symlink(lnk, dst)
+            os.lchown(dst, stats[ST_UID], stats[ST_GID])
+        else:
+            # Create hard link in destination.
+            odst = re.sub(self.old, self.dst, link)
+            os.link(dst, odst)
+
+def sortbackups(path):
+    """Returns a sorted list of Time Machine backups, with certain
+       entries pruned from the list (Latest and *.inProgress)."""
+    entries = os.listdir(path)
+    okay = lambda s: s != "Latest" and not s.endswith(".inProgress")
+    entries = [entry for entry in entries if okay(entry)]
+    entries.sort()
+    return entries
+
+def usage():
+    print "Usage: timecopy.py [-hv] <src> <dst>"
+    print ""
+    print "Copies a Mac OS X Time Machine volume (set of backups) from one location"
+    print "to another, such as from one disk to another, or from one disk image to"
+    print "another. This can be useful when block copying the disk is not feasible"
+    print "(i.e. the destination disk is smaller than the original)."
+    print ""
+    print "The given <src> location must be the root directory of the source Time"
+    print "Machine volume, that which contains the 'Backups.backupdb' directory"
+    print "(e.g. /Volumes/Backup, not /Volumes/Backup/Backups.backupdb/gojira)."
+    print "You must have sufficient privileges to access this directory, and the"
+    print "Time Machine volume must already be mounted (read-only mode is okay)."
+    print ""
+    print "The <dst> location is (ideally) the root of an empty volume to which the"
+    print "Time Machine backups will be copied. You must have sufficient privileges"
+    print "to write to this location. Chances are you will need to be using `sudo`"
+    print "to gain the necessary privileges, unless -n or --dry-run is given."
+    print ""
+    print "-h|--help"
+    print "\tPrints this usage information."
+    print ""
+    print "-n|--dry-run"
+    print "\tDo not make any changes to the destination volume."
+    print "\tTODO: NOT YET IMPLEMENTED"
+    print ""
+    print "-v|--verbose"
+    print "\tPrints information about what the script is doing at each step."
+    print "\tTODO: NOT YET IMPLEMENTED"
+
+def main():
+    """The main program method which handles user input and kicks off
+       the copying process."""
+
+    # Parse the command line arguments.
+    shortopts = "hnv"
+    longopts = ["help", "dry-run" "verbose"]
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], shortopts, longopts)
+    except getopt.GetoptError, err:
+        print str(err)
+        sys.exit(2)
+    verbose = False
+    dryrun = False
+    for o, v in opts:
+        if o in ("-v", "--verbose"):
+            verbose = True
+        elif o in ("-h", "--help"):
+            usage()
+            sys.exit()
+        elif o in ("-n", "--dry-run"):
+            dryrun = True
+        else:
+            assert False, "unhandled option"
+    if len(args) != 2:
+        usage()
+        sys.exit(2)
+    src = args[1]
+    dst = args[2]
+    mode = os.lstat(src)[ST_MODE]
+    if S_ISDIR(mode):
+        # Create destination if necessary.
+        if not os.path.exists(dst):
+            os.makedirs(dir)
+        # TODO: copy bookkeeping files -- is it really necessary?
+        # TODO: copy backups
+        pass
+    else:
+        print '%s is not a directory!' % src
 
 if __name__ == '__main__':
-    print 'THIS IS A WORK IN PROGRESS, DO NOT EXPECT IT TO WORK!'
-    if len(sys.argv) != 2:
-        print 'Usage: timecopy.py <path>'
-    else:
-        path = sys.argv[1]
-        mode = os.lstat(path)[ST_MODE]
-        if S_ISDIR(mode):
-            walktree(path, BasicTreeVisitor())
-        else:
-            print '%s is not a directory!' % path
+    print 'THIS IS A WORK IN PROGRESS, DO NOT EXPECT IT TO WORK! (XXX)'
+    main()
